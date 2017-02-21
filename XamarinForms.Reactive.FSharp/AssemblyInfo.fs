3df5263ef99211342be22910f94e559d22ee0525
Rob Lyndon <rob.lyndon@gmail.com>
2017-02-21 08:21:27 +0000

    NuGet update

--- XamarinForms.Reactive.FSharp/AssemblyInfo.fs
+++ XamarinForms.Reactive.FSharp/AssemblyInfo.fs
@@ -2,8 +2,6 @@
 
 open System.Resources
 open System.Reflection
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -28,7 +26,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyInformationalVersion("0.2.13-alpha")>]
+[<assembly: AssemblyInformationalVersion("0.2.15-alpha")>]
 
 do
     ()
