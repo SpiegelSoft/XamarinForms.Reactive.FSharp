4811b67ba77721ebd01c9d47d6235d09ac15a67f
Rob Lyndon <rob.lyndon@gmail.com>
2017-03-25 23:20:18 +0000

    Removed old common project

--- XamarinForms.Reactive.Samples.Shared/Models.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace XamarinForms.Reactive.Samples.Shared
-
-open XamarinForms.Reactive.FSharp
-
-open System
-
-type Person = { Name: string; DateOfBirth: DateTime }
-
-type ICustomPlatform = inherit IPlatform
