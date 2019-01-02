a766c0e71c9529e187b4846fc647b42a3775c571
Rob Lyndon <rob.lyndon@gmail.com>
2019-01-02 19:17:51 +0000

    Fix for FSharp.Core NuGet package

--- XamarinForms.Reactive.FSharp.Legacy/Platform.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace XamarinForms.Reactive.FSharp
-
-open Xamarin.Forms
-
-open Splat
-
-type IPlatform = 
-    abstract member RegisterDependencies: dependencyResolver:IMutableDependencyResolver -> unit
-    abstract member GetLocalFilePath: fileName:string -> string
