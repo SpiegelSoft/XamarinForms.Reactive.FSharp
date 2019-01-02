a1d6d72ec9569ef0f9d76bc9f3ca64b8e558028d
Rob Lyndon <rob.lyndon@gmail.com>
2019-01-02 19:01:39 +0000

    DevOps for AndroidManifest

---
+++ XamarinForms.Reactive.Sample.Mars.Android/appcenter-pre-build.sh
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/env bash
+#
+# For Xamarin Android or iOS, change the package name located in AndroidManifest.xml and Info.plist. 
+# AN IMPORTANT THING: YOU NEED DECLARE PACKAGE_NAME ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION.
+
+if [ ! -n "$NASA_API_KEY" ]
+then
+    echo "You need define the PACKAGE_NAME variable in App Center"
+    exit
+fi
+
+ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/Droid/Properties/AndroidManifest.xml
+
+if [ -e "$ANDROID_MANIFEST_FILE" ]
+then
+    echo "Updating NASA API key to $NASA_API_KEY in AndroidManifest.xml"
+    sed -i '' 's/meta-data android:name="nasa-api-key" android:value="[a-zA-Z0-9]*"/meta-data android:name="nasa-api-key" android:value="'$NASA_API_KEY'"/' $ANDROID_MANIFEST_FILE
+
+    echo "File content:"
+    cat $ANDROID_MANIFEST_FILE
+fi
+
